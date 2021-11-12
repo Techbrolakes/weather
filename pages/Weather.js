453c202140bd098ce29646b1e112f6b72fe52992
Lekan Dar <Lekandar11@gmail.com>
2021-11-11 20:22:40 -1200

    Version-1

---
+++ pages/Weather.js
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import DisplayWeather from "./components/DisplayWeather";
+
+function Weather() {
+  const [weather, setWeather] = useState([]);
+  const [form, setForm] = useState({
+    city: "",
+    country: "",
+  });
+
+  async function weatherData(e) {
+    e.preventDefault();
+    if (form.city == "") {
+      alert("Add values");
+    } else {
+      const data = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${form.city},${form.country}&APPID=3684c2f87ef57d6a97bf0acdf918a55c`
+      )
+        .then((res) => res.json())
+        .then((data) => data);
+
+      setWeather({ data: data });
+    }
+  }
+
+  const handleChange = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    if (name == "city") {
+      setForm({ ...form, city: value });
+    }
+    if (name == "country") {
+      setForm({ ...form, country: value });
+    }
+  };
+  return (
+    <Container>
+      <Heading>
+        <h1>Lakeside Weather</h1>
+      </Heading>
+      <br />
+      <Form>
+        <input
+          autocomplete="off"
+          type="text"
+          placeholder="Select City"
+          name="city"
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          autocomplete="off"
+          type="text"
+          placeholder="Select Country"
+          name="country"
+          onChange={(e) => handleChange(e)}
+        />
+        <Button className="getweather" onClick={(e) => weatherData(e)}>
+          Submit
+        </Button>
+      </Form>
+
+      {/* {console.log(weather)} */}
+      {weather.data != undefined ? (
+        <div>
+          <DisplayWeather data={weather.data} />
+        </div>
+      ) : null}
+    </Container>
+  );
+}
+
+export default Weather;
+
+const Container = styled.div`
+  width: 100vw;
+`;
+
+const Heading = styled.div`
+  text-align: center;
+  @media all and (min-width: 320px) and (max-width: 600px) {
+    font-size: 15px;
+  }
+`;
+
+const Form = styled.div`
+  text-align: center;
+
+  input[type="text"] {
+    padding: 10px;
+    margin: 0 10px;
+    box-shadow: 0 0 15px 4px rgba(0, 0, 0, 0.06);
+    border: none;
+    border-radius: 10px;
+    font-size: 18px;
+
+    @media all and (min-width: 320px) and (max-width: 600px) {
+      margin: 20px 5px;
+    }
+  }
+`;
+
+const Button = styled.div`
+  height: 40px;
+  cursor: pointer;
+  border-radius: 100px;
+  opacity: 0.85;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  text-align: center;
+  background-color: rgba(23, 26, 32, 0.8);
+  color: white;
+  text-transform: uppercase;
+  margin: 20px auto;
+  transition: all 0.4s ease-in;
+  :hover {
+    background-color: #dc2730;
+    opacity: 0.8;
+  }
+`;
---
+++ pages/components/DisplayWeather.js
@@ -0,0 +1,172 @@
+import React from "react";
+import styled from "styled-components";
+
+function DisplayWeather(props) {
+  const { data } = props;
+  const iconurl =
+    "http://openweathermap.org/img/wn/" +
+    `${data.cod != 404 ? data.weather[0].icon : null}` +
+    ".png";
+  return (
+    <Container>
+      {data.cod != 404 ? (
+        <React.Fragment>
+          <Card>
+            <CardLocation>
+              <span>
+                The weather in {data.name} , {data.sys.country}.
+              </span>
+              <span> As of {new Date().toLocaleTimeString()} is</span>
+            </CardLocation>
+            <CardIcon>
+              <div>
+                <h1>
+                  {Math.floor(data.main.temp - 273.15)}
+                  <sup>o</sup>
+                </h1>
+              </div>
+              <div>
+                <span>{data.weather[0].main}</span>
+                <img src={iconurl} alt="" srcset="" />
+                <span> {data.weather[0].description}</span>
+              </div>
+            </CardIcon>
+          </Card>
+
+          <TempInfo>
+            <div>
+              <Info>
+                {" "}
+                <h4>High/Low</h4>
+                <span>
+                  {Math.floor(data.main.temp_max - 273.15)}/
+                  {Math.floor(data.main.temp_min - 273.15)}
+                </span>
+              </Info>
+              <Info>
+                {" "}
+                <h4>Humidity</h4>
+                <span>{data.main.humidity} %</span>
+              </Info>
+
+              <Info>
+                {" "}
+                <h4>Pressure</h4>
+                <span>{data.main.pressure} hPa</span>
+              </Info>
+              <Info>
+                {" "}
+                <h4>Visibility</h4>
+                <span>{data.visibility / 1000} Km</span>
+              </Info>
+            </div>
+
+            <div>
+              <Info>
+                <h4>Wind</h4>
+                <span>{Math.floor((data.wind.speed * 18) / 5)} km/hr</span>
+              </Info>
+              <Info>
+                <h4>Wind Direction</h4>
+                <span>
+                  {data.wind.deg}
+                  <sup>o</sup> deg
+                </span>
+              </Info>
+              <Info>
+                <h4>Sunrise</h4>
+                <span>
+                  {new Date(data.sys.sunrise * 1000).toLocaleTimeString()}
+                </span>
+              </Info>
+              <Info>
+                <h4>Sunset</h4>
+                <span>
+                  {new Date(data.sys.sunset * 1000).toLocaleTimeString()}
+                </span>
+              </Info>
+            </div>
+          </TempInfo>
+        </React.Fragment>
+      ) : (
+        <div className="maincard">
+          <h2>{data.message}</h2>
+        </div>
+      )}
+    </Container>
+  );
+}
+
+export default DisplayWeather;
+
+const Container = styled.div`
+  width: 70vw;
+  margin: 20px auto;
+  text-align: center;
+  box-shadow: 0 0 15px 4px rgba(0, 0, 0, 0.06);
+  padding: 22px 30px;
+
+  @media screen and (max-width: 768px) {
+    width: 100vw;
+  }
+`;
+const Card = styled.div`
+  margin: 0 auto;
+  border-bottom: 2px solid black;
+`;
+const CardLocation = styled.div`
+  text-align: center;
+`;
+const CardIcon = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 20vw;
+  margin: 0 auto;
+  text-align: center;
+
+  @media all and (min-width: 320px) and (max-width: 600px) {
+    flex-direction: column;
+    justify-content: center;
+  }
+  @media screen and (min-width: 601px) and (max-width: 768px) {
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const TempInfo = styled.div`
+  display: flex;
+  justify-content: space-around;
+
+  @media all and (min-width: 320px) and (max-width: 600px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  @media screen and (min-width: 601px) and (max-width: 768px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+const Info = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 15vw;
+  border-bottom: 1px solid black;
+
+  @media all and (min-width: 320px) and (max-width: 600px) {
+    flex-direction: column;
+    width: 90vw;
+    justify-content: center;
+  }
+  @media screen and (min-width: 601px) and (max-width: 768px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 90vw;
+  }
+`;
